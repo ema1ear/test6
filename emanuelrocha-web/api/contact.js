f47f517cb77b5c9e0b99913f956751c7314bdf91
ema1ear <157844299+ema1ear@users.noreply.github.com>
2026-07-19 14:02:46 +0200

    Delete emanuelrocha-web directory

--- emanuelrocha-web/api/contact.js
+++
@@ -1,5 +0,0 @@
-import { handleContact } from "../server/contact-handler.js";
-
-export function POST(request) {
-    return handleContact(request, process.env);
-}
--- emanuelrocha-web/functions/api/contact.js
+++
@@ -1,5 +0,0 @@
-import { handleContact } from "../../server/contact-handler.js";
-
-export function onRequestPost({ request, env }) {
-    return handleContact(request, env);
-}
